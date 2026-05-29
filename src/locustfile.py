30d6bc2d98f6505e8533d13bfa32b1c32e9b540f
LleilaA13 <zanoni.2033176@studenti.uniroma1.it>
2026-05-29 16:07:43 +0200

    Added ShortWorkload (5-minute duration)(from 10 users to 50) to locustfile.py to isolate and compare the execution duration of LightGBM vs. XGBoost_arpa vs. XGBoost_baseline on serverless infrastructure.

--- src/locustfile.py
+++ src/locustfile.py
@@ -12,7 +12,7 @@ class AQIPredictorUser(HttpUser):
         "features": [45.2, 78.1, 32.5, 1.2, 8.3, 65.0, 22.1, 58.0, 3.2, 1013.0, 0.0, 0.0]
     }
 
-    @task(1)
+    # @task(1)
     def test_xgb_arpa(self):
         # 12 feature per ARPA
         self.client.post("/predict/xgb_arpa", json=self.payload_xgb)
@@ -22,12 +22,13 @@ class AQIPredictorUser(HttpUser):
         # USA payload_lgbm CHE HA 11 FEATURE!
         self.client.post("/predict/xgb_baseline", json=self.payload_lgbm)
 
-    @task(2)
+    # @task(1)
     def test_lgbm(self):
         # 11 feature per LGBM
         self.client.post("/predict/lgbm", json=self.payload_lgbm)
 
 
+'''
 class BurstWorkload(LoadTestShape):
     stages = [
         # Phase 1: Baseline (5m) - 5 users
@@ -45,4 +46,22 @@ class BurstWorkload(LoadTestShape):
         for stage in self.stages:
             if run_time < stage["duration"]:
                 return stage["users"], stage["spawn_rate"]
+        return None
+'''
+
+
+class ShortWorkload(LoadTestShape):
+    stages = [
+        # Fase 1: Riscaldamento (primi 30 secondi) - 10 utenti
+        {"duration": 30, "users": 10, "spawn_rate": 2},
+        # Fase 2: Carico costante (fino a 5 minuti / 300 secondi) - 30 utenti
+        {"duration": 300, "users": 50, "spawn_rate": 5},
+    ]
+
+    def tick(self):
+        run_time = self.get_run_time()
+        for stage in self.stages:
+            if run_time < stage["duration"]:
+                return stage["users"], stage["spawn_rate"]
+        # Il test si fermerà automaticamente dopo 300 secondi (5 minuti)
         return None
